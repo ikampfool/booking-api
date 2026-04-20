2736aa2ecd6860d81b2b055a99a15fad0673b5d8
Kopchai Worawate <ikamp@Kopchais-MacBook-Pro.local>
2026-04-20 14:56:50 +0700

    fixed dup schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingModule } from './booking/booking.module';
 import { EventModule } from './event/event.module';
+import { Booking } from './booking/booking.entity';
+import { Event } from './event/event.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -12,11 +18,18 @@ import { EventModule } from './event/event.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      autoLoadEntities: true,
+      entities: [Booking, Event],
       synchronize: true,
+      logging: true
     }),
     BookingModule,
     EventModule
   ],
 })
-export class AppModule {}
+export class AppModule {
+  // constructor() {
+  //   console.log('DB_HOST:', process.env.DB_HOST);
+  //   console.log('DB_PORT:', process.env.DB_PORT);
+  //   console.log('DB_NAME:', process.env.DB_NAME);
+  // }
+}
