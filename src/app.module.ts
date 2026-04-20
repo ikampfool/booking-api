41462f7b5532862dc723cdf42e5e379d822b863d
Kopchai Worawate <ikamp@Kopchais-MacBook-Pro.local>
2026-04-20 13:03:09 +0700

    use db information from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,11 @@ import { EventModule } from './event/event.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: '1234',
-      database: 'booking',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
